5d786162c84a354ab7f64f1f61ea4be0f6284fe6
Nathaniel Jones <nathanieljones@Nathaniels-MacBook-Pro.local>
2015-01-23 08:16:56 -0800

    Initial Commit

---
+++ iGeo_iCoreDescription/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iGeo_iCoreDescription
+//
+//  Created by Nathaniel Jones on 1/23/15.
+//  Copyright (c) 2015 Nate Geo LLC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iGeo_iCoreDescription/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iGeo_iCoreDescription
+//
+//  Created by Nathaniel Jones on 1/23/15.
+//  Copyright (c) 2015 Nate Geo LLC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
